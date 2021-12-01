3fb0a3b0dfbddb217ddc86cee63906d2b9d01868
FidaNourin13 <91682320+FidaNourin13@users.noreply.github.com>
2021-12-01 14:13:30 +0530

    Add files via upload

---
+++ larg.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+  {
+	int a,b;
+	printf("enter two numbers");
+	scanf("%d %d",&a,&b);
+   if(a>b)
+      {
+          printf("%d is largest",a);
+      }
+   else
+       {
+              printf("%d is largest",b);
+       
+}
+   }
